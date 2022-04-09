d6ddae160106e0b640480de2df1ddec62f3411f1
Cesar Rodriguez <cesarodriguez4@gmail.com>
2022-04-09 12:09:56 -0400

    lazy loading images

--- client/src/components/MovieGallery.js
+++ client/src/components/MovieGallery.js
@@ -6,7 +6,7 @@ export const MovieGallery = ({movies}) => {
             {
                 movies.map(movie => (
                     <div className="flex-1 p-2" key={movie.id}>
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                        <img loading="lazy" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                         <div className="p-2">
                             <h2 className="text-2xl font-bold">{movie.title}</h2>
                             <p className="text-sm">Release: {movie.release_date}</p>
